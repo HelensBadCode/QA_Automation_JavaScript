e86848b5a202dffbd186d2255818cff31d4534e6
Olena Turcheniuk <111610861+HelensBadCode@users.noreply.github.com>
2024-01-30 20:06:26 +0200

    Update task1.js

--- jsBasics/homeWork5/task1.js
+++ jsBasics/homeWork5/task1.js
@@ -1,22 +1,22 @@
 //function declaration
-function calculationAreaOfRectangle(width, height){
+function calculateRectangleArea(width, height){
     const result = width * height;
     return result;
 }
-console.log("The area of the rectangle :", calculationAreaOfRectangle(23, 16));
+console.log("The area of the rectangle :", calculateRectangleArea(23, 16));
 //----------------------------------------------------------------------------------------------------------------------
 
 //function expression
-const calculationAreaOfRectangle2 = function (width, height) {
+const calculateRectangleArea2 = function (width, height) {
     const result = width * height;
     return result;
 }
-console.log("The area of the rectangle :",calculationAreaOfRectangle2(26, 32));
+console.log("The area of the rectangle :",calculateRectangleArea2(26, 32));
 //----------------------------------------------------------------------------------------------------------------------
 
 //arrow function expression
-const calculationAreaOfRectangle3 = (width, height) => {
+const calculateRectangleArea3 = (width, height) => {
     const result = width * height;
     return result;
 }
-console.log("The area of the rectangle :",calculationAreaOfRectangle3(33, 12));
+console.log("The area of the rectangle :",calculateRectangleArea3(33, 12));
