7a4214ee662ea3c5e784d82802d7110d551cab07
Olena Turcheniuk <e.turcheniuk21@gmail.com>
2024-02-01 22:03:08 +0200

    lesson7

---
+++ Lessons/callback.js
@@ -0,0 +1,30 @@
+//callback - функція яка передається в іншу функцію в якості аргументу
+// function hot (callback){
+//     console.log("High order function!");
+//     callback()//функція яка передається в іншу функцію в якості аргументу
+// }
+//
+// const sayHello = () => console.log("Hello from Callback");
+// hot(sayHello);
+
+function processPayment(status1, status2){
+    try{
+        if(Date.now() %2 === 0){
+            throw new Error("Error payment!")
+
+        }
+        status1()
+    } catch (e){
+        status2(e)
+    }
+}
+
+function onPaymentSuccess(){
+    console.log("Payment has been processed!");
+}
+
+function onPaymentError(error){
+    console.log(`Payment error: ${error.message}`);
+}
+
+processPayment(onPaymentSuccess,onPaymentError);
---
+++ Lessons/this.js
@@ -0,0 +1,36 @@
+// const person = {
+//     name : 'Alise',
+//     greet : function (){
+//         console.log(`Hello ${this.name}`);
+//     }
+// };
+// person.greet();
+
+// // стрілочні ф-ції не використовуються!
+// const person = {
+//     name : 'Alise',
+//     greet : function => {
+//         console.log(`Hello ${this.name}`);
+//     }
+// };
+// const person = {
+//     name : "Ivan"
+// }
+// const person2 = {
+//     name : 'Piter',
+//     greet : function() {
+//     console.log(`Hello ${this.name}`);
+//    },
+//     sayHello (greetigText){
+//         console.log(`${greetigText}, ${this.name}`);
+//     }
+// };
+//
+// person2.greet();
+// person2.sayHello("Hola");
+// const greetFunction = person2.sayHello.bind(person);
+// greetFunction("Privit");
+// person2.greet.call(person);
+
+const numbers =[1, 3, 5, 7];
+
