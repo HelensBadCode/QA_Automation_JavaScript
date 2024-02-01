5e0804869a159fa534c03d7554036fca3acd8434
Olena Turcheniuk <e.turcheniuk21@gmail.com>
2024-02-01 23:27:02 +0200

    Homework6 for lesson7

---
+++ jsBasics/homeWork6/task1.js
@@ -0,0 +1,22 @@
+ function handleNum (number, handleEven, handleOdd) {
+     if (number === 0) {
+         console.log("Number can not be 0!");
+     } else if (number % 2 === 0) {
+         handleEven();
+     } else if (number % 2 !== 0) {
+         handleOdd();
+     }
+ }
+
+function handleEven() {
+    console.log("Number is even.");
+}
+
+ function handleOdd() {
+     console.log("Number is odd.");
+ }
+
+ handleNum(0, handleEven, handleOdd);
+ handleNum(5, handleEven, handleOdd);
+ handleNum(222, handleEven, handleOdd);
+
---
+++ jsBasics/homeWork6/task2.js
@@ -0,0 +1,7 @@
+function countToZero(num){
+    console.log(num)
+    if(num > 0) {
+        countToZero(num - 1)
+    }
+}
+countToZero(5);
