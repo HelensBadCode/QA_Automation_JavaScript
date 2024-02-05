617c584a616e1d1297524c81a5684c4d6f54842b
Olena Turcheniuk <e.turcheniuk21@gmail.com>
2024-02-05 23:27:14 +0200

    Homework7 for  Lesson8

---
+++ jsBasics/homeWork7/task6.js
@@ -0,0 +1,8 @@
+const numbersList = [1,10,14,2,4,5,43,34];
+const copyArray = structuredClone(numbersList);
+console.log("Copy array before sort :", copyArray);
+copyArray.sort((a, b) => a- b);
+
+console.log("Original array :", numbersList);
+console.log("Copy array after sort :", copyArray);
+
