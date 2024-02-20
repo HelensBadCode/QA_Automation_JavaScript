323b5d430bc68ff02b4e149a9899a30117dda18c
Olena Turcheniuk <e.turcheniuk21@gmail.com>
2024-02-20 09:23:49 +0200

    notice

---
+++ Lessons/static.js
@@ -0,0 +1,20 @@
+class MyMath {
+    static PI = 3
+
+    static getCircleArea(radius){//можна викликати тільки з самого класу, але не з екземплярів
+        return radius * this.PI
+    }
+}
+
+const math = new MyMath()
+// console.log(math.PI)//undefined
+console.log(MyMath.PI)
+console.log(MyMath.getCircleArea(10))
+
+class DateTimeHelper {
+    static getCurrentDay(){
+        return new Date().getDate()
+    }
+}
+
+console.log(DateTimeHelper.getCurrentDay())
